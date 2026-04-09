cc4b5ad6079d0b32bc17a54e17a6081021c68c23
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-04-10 00:29:48 +0600

    clear history button event handle complate

---
+++ script/clearHistory.js
@@ -0,0 +1,4 @@
+document.getElementById("clear-history-btn").addEventListener("click", function(){
+ const addListContainer = document.getElementById("add-list");
+ addListContainer.remove("add-list")
+})
