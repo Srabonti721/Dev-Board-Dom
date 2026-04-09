ac2983b79b853e48ec9838b29a138f4eec4ffff4
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-04-10 00:58:45 +0600

    added discover button event lisener

---
+++ script/discover.js
@@ -0,0 +1,8 @@
+document.getElementById("discover").addEventListener("click", function(){
+    window.location.href = "./question.html"   
+})
+document.getElementById("back-desk-btn").addEventListener("click", function(){
+    // console.log("loged");
+    
+    window.location.href = "index.html"   
+})
