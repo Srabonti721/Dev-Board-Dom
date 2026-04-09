9db3ed8a48dfede7ca6d7a49eda7115f78bc05f6
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-04-09 23:58:02 +0600

    complate button add even handler complate

---
+++ script/complete.js
@@ -0,0 +1,38 @@
+  const title1 = document.getElementById("fix-mobile-title").innerText;
+     const btnCompleted1 = document.getElementById("complete-btn1");
+document.getElementById("complete-btn1").addEventListener("click", function(){
+    completedButton(title1, btnCompleted1)
+})
+
+
+   const title2 = document.getElementById("dark-mode").innerText;
+      const btnCompleted2 = document.getElementById("complete-btn2");
+document.getElementById("complete-btn2").addEventListener("click",function(){
+completedButton(title2, btnCompleted2)
+
+} )
+   const title3 = document.getElementById("Optimize").innerText;
+      const btnCompleted3 = document.getElementById("complete-btn3");
+document.getElementById("complete-btn3").addEventListener("click",function(){
+completedButton(title3, btnCompleted3)
+
+} )
+   const title4 = document.getElementById("emoji").innerText;
+      const btnCompleted4 = document.getElementById("complete-btn4");
+document.getElementById("complete-btn4").addEventListener("click",function(){
+completedButton(title4, btnCompleted4)
+
+} )
+   const title5 = document.getElementById("Integrate").innerText;
+      const btnCompleted5 = document.getElementById("complete-btn5");
+document.getElementById("complete-btn5").addEventListener("click",function(){
+completedButton(title5, btnCompleted5)
+
+} )
+   const title6 = document.getElementById("Improve").innerText;
+      const btnCompleted6 = document.getElementById("complete-btn6");
+document.getElementById("complete-btn6").addEventListener("click",function(){
+completedButton(title6, btnCompleted6)
+
+} )
+
---
+++ script/utility.js
@@ -0,0 +1,34 @@
+function getIdBYConvertedValue(id) {
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseInt(value);
+    return convertedValue;
+}
+
+function setInnerTextByIdAndValue(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+function completedButton(title, btnCompleted) {
+    alert("Board update successfully");
+    const taskNumber = getIdBYConvertedValue("task-number");
+    const totalNumber = getIdBYConvertedValue("total-number");
+    if (taskNumber > 0 && totalNumber >= 23) {
+        const add = taskNumber - 1;
+         const diff= totalNumber + 1;
+        setInnerTextByIdAndValue("task-number", add)
+        setInnerTextByIdAndValue("total-number", diff);
+        //   add massage
+        const addListContainer = document.getElementById("add-list");
+        const p = document.createElement("p");
+        p.classList.add("style");
+        p.innerText = `
+      You have completed the task ${title} at 
+     `
+        addListContainer.appendChild(p);
+        // btn Disabled
+        btnCompleted.setAttribute("disabled", false)
+    }
+    else {
+        console.log("invalid value");
+    }
+}
